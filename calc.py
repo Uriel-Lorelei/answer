54fc1f023c2eb77c2894aa7c6342512bfc950d89
Lorelei <uriel26535897932384626433@proton.me>
2025-12-17 09:26:27 +0545

    calc

--- calc.py
+++ calc.py
@@ -1,1 +1,14 @@
-import sympy
+#!/usr/bin/env python3
+
+import sys
+from sympy.parsing.sympy_parser import (
+    parse_expr, standard_transformations, implicit_multiplication_application
+)
+
+expr = sys.stdin.read().strip()
+
+try:
+    result = parse_expr(expr, transformations=standard_transformations + (implicit_multiplication_application,))
+    print(result)
+except Exception:
+    pass
