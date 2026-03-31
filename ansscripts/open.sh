5883c90a8698f9d3cf37fe6162adbcca7c091b85
Uriel-Lorelei <somebodyuseless544@gmail.com>
2026-03-31 20:40:55 +0545

    improved dictionary

--- ansscripts/open.sh
+++ ansscripts/open.sh
@@ -1,8 +1,8 @@
 #!usr/bin/env bash
 
-kitty -e python3 ~/.config/ansscripts/dictionary.py &
-sleep 0.3
-hyprctl dispatch focuswindow title:python3
-hyprctl dispatch togglefloating
-hyprctl dispatch centerwindow
-hyprctl dispatch resizeactive exact 300 75
+kitty --title "Dictionary" -e python3 ~/.config/ansscripts/dictionary.py &
+# sleep 0.4
+hyprctl dispatch focuswindow title:Dictionary
+# hyprctl dispatch togglefloating
+# hyprctl dispatch centerwindow
+# hyprctl dispatch resizeactive exact 300 75
